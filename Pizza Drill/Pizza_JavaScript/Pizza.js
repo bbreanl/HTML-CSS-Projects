e890f5a6de245a03a90de9f452b65566f93dbf03
bbreanl <b.lewis1980@outlook.com>
2022-10-24 20:11:12 -0500

    push

--- Pizza Drill/Pizza_JavaScript/Pizza.js
+++ Pizza Drill/Pizza_JavaScript/Pizza.js
@@ -26,6 +26,32 @@ function getReceipt() {
     console.log(selectedSize+" = $"+sizeTotal+ ".00");
     console.log("size text1: "+text1);
     console.log("subtotal: $"+runningTotal+".00")
-    //these variables will get passed on to each function
-    getTopping(runningTotal,text1);
+   getTopping(runningTotal,text1); // All three of these variables will be passed on to each function 
 };
+function getTopping (runningTotal,text1) {
+    var toppingTotal = 0;
+    var selectedtopping = [];
+    var toppingArray = document.getElementsByClassName ("toppings");
+    for (var j = 0; j < toppingArray.length; j++)
+{
+  if (toppingArray[j].checked) {
+selectedTopping.push(toppingArray[j].value);
+  console.log("selected topping item: ("+toppingArray[j].value+")");
+  text1 = text1+toppingArray[j].value+"<br>";
+}
+}
+var toppingCount = selectedTopping.length;
+if (toppingCount > 1) {
+    toppingTotal = (toppingCount - 1);
+} else {
+    toppingTotal = 0;
+}
+runningTotal = (runningTotal + toppingTotal);
+console.log("total slected topping - 1 free topping = "+"$"+toppingTotal+".00");
+console.log("topping text1: "+text1);
+console.log("Purchase Total:"+"$"+runningTotal+".00");
+
+document.getElementById("showText") .innerHTML=text1;
+
+document.getElementById("totalPrice") .innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong> </h3>";
+}; 
